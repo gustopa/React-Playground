aecd167d1fee7aede5c19f133867436fbda3e57e
galdiius <gustopaMI@gmail.com>
2024-12-24 05:28:18 +0700

    initialize location-finder

---
+++ location-finder/src/App.jsx
@@ -0,0 +1,9 @@
+import MapFinder from "./components/MapFinder";
+import './style.css'
+export default function App() {
+  return (
+    <>
+      <MapFinder/>
+    </>
+  );
+}
---
+++ location-finder/src/components/Fly.jsx
@@ -0,0 +1,7 @@
+export default function Fly({setLangitude,setLongitude}){
+    return (
+        <>
+            
+        </>
+    )
+}
